d5bc6ab989fe47c107320c4db870b18703fcfd6b
GonzalezLE <gonzalez-3216@hotmail.com>
2023-07-24 23:48:19 -0600

    prueba upload files

---
+++ main.py
@@ -0,0 +1,27 @@
+#pip install google-api-python-client
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SERVICE_ACCOUNT_FILE = 'service_account.json'
+PARENT_FOLDER_ID = "1eH3B8_V1_4Ic6C3q_FOn7R58s3GXzE3P"
+
+def authenticate():
+    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    return creds
+
+def upload_photo(file_path):
+    creds = authenticate()
+    service = build('drive', 'v3', credentials=creds)
+
+    file_metadata = {
+        'name' : "Demonstration",
+        'parents' : [PARENT_FOLDER_ID]
+    }
+
+    file = service.files().create(
+        body=file_metadata,
+        media_body=file_path
+    ).execute()
+
+upload_photo("platzi.png")
